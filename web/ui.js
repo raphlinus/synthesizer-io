ce8f1182acdcb173335d2ea27e94bd8f8cea5f61
Raph Levien <raph@google.com>
2018-07-04 23:38:51 -0700

    Draw modules in addition to wires
    Adds UI for placing modules, detecting collisions.

--- web/ui.js
+++ web/ui.js
@@ -26,10 +26,20 @@ function y_of(xy) {
     return xy >> 16;
 }
 
+// create an SVG element with
+function svg_el(tag, pe = 'none') {
+    let el = document.createElementNS(NS, tag);
+    el.setAttribute('pointer-events', pe);
+    return el;
+}
+
 class Ui {
     constructor(el) {
         this.el = el;
-        this.wireGrid = new WireGrid(48, 32);
+        let grid = new Grid(48, 32);
+        this.wireGrid = new WireGrid(grid);
+        this.moduleGrid = new ModuleGrid(grid);
+        this.handler = this.wireGrid;
     }
 
     init() {
@@ -42,16 +52,35 @@ class Ui {
             self.circle.setAttribute('cy', ev.offsetY);
         };
 
-        this.el.onmousedown = function(ev) {
-            self.wireGrid.onmousedown(ev);
-        }
-        this.el.onmouseup = function(ev) {
-            self.wireGrid.onmouseup(ev);
-        }
-        this.el.onmousemove = function(ev) {
-            self.wireGrid.onmousemove(ev);
-        }
         this.wireGrid.attach(this.el);
+        this.moduleGrid.attach(this.el);
+
+        let rect = this.wireGrid.rect;
+        rect.onmousedown = function(ev) {
+            self.handler.onmousedown(ev);
+            ev.preventDefault();
+        }
+        rect.onmouseup = function(ev) {
+            self.handler.onmouseup(ev);
+            ev.preventDefault();
+        }
+        rect.onmousemove = function(ev) {
+            self.handler.onmousemove(ev);
+            ev.preventDefault();
+        }
+
+        // set up buttons
+        let button = new Button(0, 550, 'wire');
+        button.onclick = function () {
+            self.handler = self.wireGrid;
+        };
+        button.attach(this.el);
+        let button2 = new Button(60, 550, 'mod');
+        button2.onclick = function () {
+            self.handler = self.moduleGrid;
+        };
+        button2.attach(this.el);
+
     }
 
     addCircle(x, y, r, color) {
@@ -63,14 +92,36 @@ class Ui {
         this.el.appendChild(circle);
         return circle;
     }
-
 }
 
-class WireGrid {
+class Grid {
     constructor(w, h) {
         this.w = w;
         this.h = h;
+        // use translate in DOM?
+        this.x0 = 0;
+        this.y0 = 0;
         this.scale = 10;
+    }
+
+    x_y_to_xy(x, y) {
+        let i = Math.floor(x / this.scale);
+        let j = Math.floor(y / this.scale);
+        if (i >= 0 && i < this.w && j >= 0 && j < this.h) {
+            return xy_of(i, j);
+        } else {
+            return -1;
+        }
+    }
+
+    ev_to_xy(ev) {
+        return this.x_y_to_xy(ev.offsetX, ev.offsetY);
+    }
+}
+
+class WireGrid {
+    constructor(grid) {
+        this.grid = grid;
         this.hset = new Set();
         this.vset = new Set();
         this.hmap = new Map();
@@ -80,36 +131,39 @@ class WireGrid {
 
     attach(parent) {
         this.parent = parent;
-        // use translate in DOM?
-        this.x0 = 0;
-        this.y0 = 0;
-        let rect = document.createElementNS(NS, 'rect');
-        rect.setAttribute('x', this.x0);
-        rect.setAttribute('y', this.y0);
-        rect.setAttribute('width', this.w * this.scale);
-        rect.setAttribute('height', this.h * this.scale);
+        let w = this.grid.w;
+        let h = this.grid.h;
+        let scale = this.grid.scale;
+        let x0 = this.grid.x0;
+        let y0 = this.grid.y0;
+        let rect = svg_el('rect', 'all');
+        rect.setAttribute('x', x0);
+        rect.setAttribute('y', y0);
+        rect.setAttribute('width', w * scale);
+        rect.setAttribute('height', h * scale);
         rect.setAttribute('fill', '#cdf');
         parent.appendChild(rect);
 
-        for (let x = 0; x <= this.w; x++) {
-            let line = document.createElementNS(NS, 'line');
-            line.setAttribute('x1', this.x0 + x * this.scale);
-            line.setAttribute('y1', this.y0);
-            line.setAttribute('x2', this.x0 + x * this.scale);
-            line.setAttribute('y2', this.y0 + this.h * this.scale);
+        for (let x = 0; x <= w; x++) {
+            let line = svg_el('line');
+            line.setAttribute('x1', x0 + x * scale);
+            line.setAttribute('y1', y0);
+            line.setAttribute('x2', x0 + x * scale);
+            line.setAttribute('y2', y0 + h * scale);
             line.setAttribute('stroke', '#8af');
             parent.appendChild(line);
         }
 
-        for (let y = 0; y <= this.h; y++) {
-            let line = document.createElementNS(NS, 'line');
-            line.setAttribute('x1', this.x0);
-            line.setAttribute('y1', this.y0 + y * this.scale);
-            line.setAttribute('x2', this.x0 + this.w * this.scale);
-            line.setAttribute('y2', this.y0 + y * this.scale);
+        for (let y = 0; y <= h; y++) {
+            let line = svg_el('line');
+            line.setAttribute('x1', x0);
+            line.setAttribute('y1', y0 + y * scale);
+            line.setAttribute('x2', x0 + w * scale);
+            line.setAttribute('y2', y0 + y * scale);
             line.setAttribute('stroke', '#8af');
             parent.appendChild(line);
         }
+        this.rect = rect;
     }
 
     isSet(x, y, isVert) {
@@ -125,13 +179,13 @@ class WireGrid {
         if (val) {
             if (set.has(xy)) { return; }
             set.add(xy);
-            let x1 = this.x0 + (x + 0.5) * this.scale;
-            let y1 = this.y0 + (y + 0.5) * this.scale;
-            let line = document.createElementNS(NS, 'line');
+            let x1 = this.grid.x0 + (x + 0.5) * this.grid.scale;
+            let y1 = this.grid.y0 + (y + 0.5) * this.grid.scale;
+            let line = svg_el('line');
             line.setAttribute('x1', x1);
             line.setAttribute('y1', y1);
-            line.setAttribute('x2', isVert ? x1 : x1 + this.scale);
-            line.setAttribute('y2', isVert ? y1 + this.scale : y1);
+            line.setAttribute('x2', isVert ? x1 : x1 + this.grid.scale);
+            line.setAttribute('y2', isVert ? y1 + this.grid.scale : y1);
             line.setAttribute('stroke', '#000');
             line.setAttribute('stroke-width', 2);
             this.parent.appendChild(line);
@@ -144,18 +198,8 @@ class WireGrid {
         }
     }
 
-    ev_to_xy(ev) {
-        let x = Math.floor((ev.offsetX - this.x0) / this.scale);
-        let y = Math.floor((ev.offsetY - this.y0) / this.scale);
-        if (x >= 0 && x < this.w && y >= 0 && y < this.h) {
-            return xy_of(x, y);
-        } else {
-            return -1;
-        }
-    }
-
     onmousedown(ev) {
-        let xy = this.ev_to_xy(ev);
+        let xy = this.grid.ev_to_xy(ev);
         if (xy >= 0) {
             this.clickState = 1;
             this.clickXy = xy;
@@ -167,7 +211,7 @@ class WireGrid {
     }
 
     onmousemove(ev) {
-        let xy = this.ev_to_xy(ev);
+        let xy = this.grid.ev_to_xy(ev);
         if (this.clickState) {
             let seg = this.computeSegment(this.clickXy, xy);
             if (seg) {
@@ -202,6 +246,156 @@ class WireGrid {
             }            
         }
         return null;
+    }
+}
+
+class Button {
+    constructor(x, y, label) {
+        let width = 50;
+        let rect = svg_el('rect', 'all');
+        rect.setAttribute('x', x);
+        rect.setAttribute('y', y);
+        rect.setAttribute('width', width);
+        rect.setAttribute('height', 20);
+        rect.setAttribute('fill', '#ddd');
+        let text = svg_el('text');
+        text.setAttribute('x', x + width / 2);
+        text.setAttribute('y', y + 15);
+        text.setAttribute('text-anchor', 'middle');
+        text.setAttribute('fill', '#000');
+        let textNode = document.createTextNode(label);
+        text.appendChild(textNode);
+
+        let self = this;
+        rect.onclick = function(ev) {
+            self.onclick();
+        }
+        this.onclick = function() { console.log('onclick not set!'); }
+        this.rect = rect;
+        this.text = text;
+    }
+
+    attach(parent) {
+        parent.appendChild(this.rect);
+        parent.appendChild(this.text);
+    }
+}
+
+class ModuleGrid {
+    constructor(grid) {
+        this.grid = grid;
+        this.guide = null;
+        this.guideWidth = 2;
+        this.guideHeight = 2;
+        this.modules = [];
+    }
+
+    attach(parent) {
+        this.parent = parent;
+    }
+
+    ev_to_xy(ev) {
+        let x0 = ev.offsetX - 0.5 * (this.grid.scale * (this.guideWidth - 1));
+        let y0 = ev.offsetY - 0.5 * (this.grid.scale * (this.guideHeight - 1));
+        return this.grid.x_y_to_xy(x0, y0);
+    }
+
+    xy_ok(xy) {
+        if (xy < 0) { return false; }
+        let x = x_of(xy);
+        let y = y_of(xy);
+        if (x + this.guideWidth > this.grid.w) { return false; }
+        if (y + this.guideHeight > this.grid.h) { return false; }
+        for (let module of this.modules) {
+            if (x + this.guideWidth >= x_of(module.xy)
+                && x_of(module.xy) + module.w >= x
+                && y + this.guideHeight >= y_of(module.xy)
+                && y_of(module.xy) + module.h >= y) { return false; }
+        }
+        return true;
+    }
+
+    onmousedown(ev) {
+        let xy = this.ev_to_xy(ev);
+        if (this.xy_ok(xy)) {
+            let rect = svg_el('rect');
+            let module = new Module(xy, this.guideWidth, this.guideHeight);
+            module.render(this.parent, this.grid);
+            this.modules.push(module);
+        }
+    }
+
+    onmouseup(ev) {
+        console.log('mod up');
+    }
+
+    onmousemove(ev) {
+        let xy = this.ev_to_xy(ev);
+        if (xy >= 0) {
+            let x0 = this.grid.x0 + x_of(xy) * this.grid.scale;
+            let y0 = this.grid.y0 + y_of(xy) * this.grid.scale;
+            if (this.guide === null) {
+                this.guide = svg_el('rect');
+                this.guide.setAttribute('width', this.grid.scale * this.guideWidth);
+                this.guide.setAttribute('height', this.grid.scale * this.guideHeight);
+                this.guide.setAttribute('fill', '#888');
+                this.guide.setAttribute('fill-opacity', 0.5);
+                this.guide.setAttribute('stroke', '#888');
+                this.parent.appendChild(this.guide)
+            }
+            if (this.xy_ok(xy)) {
+                this.guide.setAttribute('fill', '#0c0');
+                this.guide.setAttribute('stroke', '#0c0');
+            } else {
+                this.guide.setAttribute('fill', '#e00');
+                this.guide.setAttribute('stroke', '#e00');
+            }
+            this.guide.setAttribute('x', x0);
+            this.guide.setAttribute('y', y0);
+        }
+        console.log('mod move');
+    }
+}
+
+class Module {
+    constructor(xy, w, h) {
+        this.xy = xy;
+        this.w = w;
+        this.h = h;
+    }
+
+    render(parent, grid) {
+        let g = svg_el('g');
+        let x = grid.x0 + x_of(this.xy) * grid.scale;
+        let y = grid.y0 + y_of(this.xy) * grid.scale;
+        g.setAttribute('transform', 'translate(' + x + ' ' + y + ')');
+        let rect = svg_el('rect');
+        rect.setAttribute('x', 0);
+        rect.setAttribute('y', 0);
+        rect.setAttribute('width', grid.scale * this.w);
+        rect.setAttribute('height', grid.scale * this.h);
+        rect.setAttribute('fill', 'none');
+        rect.setAttribute('stroke', '#000');
+        g.appendChild(rect);
+
+        for (let y = 0; y < this.h; y++) {
+            for (let side = 0; side < 2; side++) {
+                let line = svg_el('line');
+                if (side == 0) {
+                    line.setAttribute('x1', -0.5 * grid.scale);
+                    line.setAttribute('x2', 0);
+                } else {
+                    line.setAttribute('x1', this.w * grid.scale);
+                    line.setAttribute('x2', (this.w + 0.5) * grid.scale);
+                }
+                line.setAttribute('y1', grid.scale * (0.5 + y));
+                line.setAttribute('y2', grid.scale * (0.5 + y));
+                line.setAttribute('stroke', '#000');
+                g.appendChild(line);
+            }
+        }
+        this.g = g;
+        parent.appendChild(g);
     }
 }
 
